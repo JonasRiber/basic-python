d3b4121bb9d1f34efa673eae1acb0642b0c460d4
JonasRiber <riber16@gmail.com>
2023-09-08 09:34:01 +0200

    "pattern"

--- src/pattern.py
+++ src/pattern.py
@@ -1,10 +1,9 @@
 
 # Print the pattern
 x = ""
-for i in range(1,11):
-    
+for i in range(1,11):  
     if i < 6:
         x = x + " *"
     else:
         x = x[:-2]
-    print(x)
+    print(x[1:])
